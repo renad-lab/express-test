d709219e06e2f39d89a1b4119ba44c86c63b9ef7
renad-lab <renad@pursuit.org>
2024-06-23 14:10:10 -0400

    updates

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const app = express();
 const fruits = require("./models/fruits"); // Assuming this exports an array of fruits
-const fruitsController = require("./controllers/fruitsController");
 
 // Basic route
 app.get("/", (req, res) => {
@@ -12,7 +11,7 @@ app.get("/", (req, res) => {
 
 // Route to get all fruits
 app.get("/fruits", (req, res) => {
-  res.json(fruits); // Return all fruits as JSON
+  res.send(fruits); // Return all fruits as JSON
 });
 
 // Route to get a specific fruit by name
@@ -25,12 +24,14 @@ app.get("/fruits/:fruitName", (req, res) => {
     (fruit) => fruit.name.toLowerCase() === fruitName
   );
 
-  //   // Check if foundFruit array is empty
-  //   if (foundFruit.length === 0) {
-  //     return res.status(404).json({ error: "Fruit not found" }); // Return 404 if no fruit found
-  //   }
-
   res.json(foundFruit); // Return found fruit(s) as JSON
+});
+
+// Route to search for fruits by family using query parameter
+app.get("/fruits/search", (req, res) => {
+  const { family, calories, fat } = req.query;
+  res.send(req.query);
+  // Filter fruits array by family
 });
 
 module.exports = app;

7ad8ad28c188f74cfc3fc4d9f98064b9c559b9ef
renad-lab <renad@pursuit.org>
2024-06-23 14:14:40 -0400

    updates

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-// app.js
+// // app.js
+
+// const express = require("express");
+// const app = express();
+// const fruits = require("./models/fruits"); // Assuming this exports an array of fruits
+
+// // Basic route
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// // Route to get all fruits
+// app.get("/fruits", (req, res) => {
+//   res.send(fruits); // Return all fruits as JSON
+// });
+
+// // Route to get a specific fruit by name
+// app.get("/fruits/:fruitName", (req, res) => {
+//   let { fruitName } = req.params;
+//   fruitName = fruitName.toLowerCase(); // Convert fruitName to lowercase
+
+//   // Filter fruits array by fruitName (case-insensitive)
+//   const foundFruit = fruits.filter(
+//     (fruit) => fruit.name.toLowerCase() === fruitName
+//   );
+
+//   res.json(foundFruit); // Return found fruit(s) as JSON
+// });
+
+// // Route to search for fruits by family using query parameter
+// app.get("/fruits/search", (req, res) => {
+//   const { family, calories, fat } = req.query;
+//   res.send(req.query);
+//   // Filter fruits array by family
+// });
+
+// module.exports = app;
 
 const express = require("express");
 const app = express();
-const fruits = require("./models/fruits"); // Assuming this exports an array of fruits
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello!");
+const fruitsController = require("./controllers/fruitsController");
+
+// HomePage
+app.get("/", (request, response) => {
+  response.send(`Hello!`);
 });
 
-// Route to get all fruits
-app.get("/fruits", (req, res) => {
-  res.send(fruits); // Return all fruits as JSON
-});
-
-// Route to get a specific fruit by name
-app.get("/fruits/:fruitName", (req, res) => {
-  let { fruitName } = req.params;
-  fruitName = fruitName.toLowerCase(); // Convert fruitName to lowercase
-
-  // Filter fruits array by fruitName (case-insensitive)
-  const foundFruit = fruits.filter(
-    (fruit) => fruit.name.toLowerCase() === fruitName
-  );
-
-  res.json(foundFruit); // Return found fruit(s) as JSON
-});
-
-// Route to search for fruits by family using query parameter
-app.get("/fruits/search", (req, res) => {
-  const { family, calories, fat } = req.query;
-  res.send(req.query);
-  // Filter fruits array by family
-});
+// Fruits Routes
+app.use("/fruits", fruitsController);
 
 module.exports = app;
--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -1,9 +1,39 @@
+// const express = require("express");
+// const fruits = express.Router();
+// const fruitsArray = require("../models/fruits");
+
+// fruits.get("/", (req, res) => {
+//   res.json(fruitsArray);
+// });
+
+// module.exports = fruits;
+
 const express = require("express");
 const fruits = express.Router();
-const fruitsArray = require("../models/fruits");
 
-fruits.get("/", (req, res) => {
-  res.json(fruitsArray);
+const fruitsData = require("../models/fruits");
+
+//     "/fruits"
+fruits.get("/", (request, response) => {
+  response.json(fruitsData);
+});
+
+// "fruits/search?calories=103"
+fruits.get("/search", (request, response) => {
+  const { calories } = request.query;
+  const fruitsWithCalories = fruitsData.filter(
+    (fruit) => Number(fruit.nutritions.calories) <= Number(calories)
+  );
+  response.json(fruitsWithCalories);
+});
+
+// "fruits/Strawberry"
+fruits.get("/:fruitName", (request, response) => {
+  const { fruitName } = request.params;
+  const foundFruit = fruitsData.filter(
+    (fruit) => fruit.name.toLowerCase() === fruitName.toLowerCase()
+  );
+  response.json(foundFruit[0]);
 });
 
 module.exports = fruits;
